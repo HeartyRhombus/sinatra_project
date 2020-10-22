6eabd31dd4264883a559dfc297979f90d5af086b
HeartyRhombus <kaylahmg@gmail.com>
2020-10-22 12:44:41 -0700

    add routes to games controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,36 @@
+class GamesController < ApplicationController
+
+# index
+    get "/games" do
+        erb :'games/index'
+    end
+
+# new
+    get "/games/new" do
+        erb :'games/new'
+    end
+
+    post "/games" do
+        
+    end
+
+#show
+    get "/games/:id" do
+        erb :"games/#{@game.id}"
+    end
+
+# edit
+    get "/games/:id/edit" do
+        erb :"games/edit"
+    end
+
+    patch "/games/:id" do
+        
+    end
+
+#delete
+    delete "/games/:id" do
+        
+    end
+
+end
