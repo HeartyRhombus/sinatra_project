734d7b17bcd19ce892d48b09d46330da5c071cd2
HeartyRhombus <kaylahmg@gmail.com>
2020-10-23 16:40:11 -0700

    update model relationships

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ActiveRecord::Base
-    belongs_to :player
+    has_and_belongs_to_many :player
     # validates :title, :platform, prescense: true
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ActiveRecord::Base
-    has_many :games
+    has_and_belongs_to_many :games
     has_secure_password
     # validates :name, :preferred_platform, presence: true
 end
