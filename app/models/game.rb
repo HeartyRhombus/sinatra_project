cccfe71e09da7590e81b9db73a8e1615a5aebd6f
HeartyRhombus <kaylahmg@gmail.com>
2020-10-23 16:49:15 -0700

    add validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ActiveRecord::Base
     has_and_belongs_to_many :player
-    # validates :title, :platform, prescense: true
+    validates :title, :platform, presence: true
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,6 @@
 class Player < ActiveRecord::Base
     has_and_belongs_to_many :games
     has_secure_password
-    # validates :name, :preferred_platform, presence: true
+    validates :email, uniqueness:true
+    validates :name, :password, presence: true
 end
