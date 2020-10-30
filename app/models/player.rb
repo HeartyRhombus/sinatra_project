5a20f1a3c1496b2293d5c1abfa8336bf72d41812
HeartyRhombus <kaylahmg@gmail.com>
2020-10-30 13:44:15 -0700

    update player_id to creator_id

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ActiveRecord::Base
     has_and_belongs_to_many :games
-    # has_many :created_games, foreign_key: "player_id", class_name: "Game"
+    has_many :created_games, foreign_key: "creator_id", class_name: "Game"
     has_secure_password
     validates :email, uniqueness:true
     validates :email, :name, :password, presence: true
