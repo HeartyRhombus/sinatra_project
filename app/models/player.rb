0a41018f07efb36c7cf31c79f42617062f43f08b
HeartyRhombus <kaylahmg@gmail.com>
2020-10-30 12:17:37 -0700

    add email to validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ActiveRecord::Base
     has_and_belongs_to_many :games
+    # has_many :created_games, foreign_key: "player_id", class_name: "Game"
     has_secure_password
     validates :email, uniqueness:true
-    validates :name, :password, presence: true
+    validates :email, :name, :password, presence: true
 end
